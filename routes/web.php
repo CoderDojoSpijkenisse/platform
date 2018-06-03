00222f0276823e4f65a3d2ac728ed70ed14aca0a
Sander van den Oever <sandervdo@gmail.com>
2018-06-03 21:07:33 +0200

    [#9] Fix phpcs error

--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,4 @@ Route::resource('event-registrations', 'EventRegistrationController');
 Route::group(['prefix' => 'parent', 'middleware' => ['auth']], function () {
     Route::get('children', 'ChildrenController@index')->name('parent.children');
     Route::post('registrations', 'ChildrenController@updateRegistrations')->name('parent.registrations');
-
 });
